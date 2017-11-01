e53784dc4d33ca1f6e4408be903551028e55be1f
Patrick Dwyer <patrick.dwyer@invincea.com>
2017-11-01 16:51:34 -0400

    Observe routes now return HTTP 200, and generate appropriate JSON. Results are validated in virtue-security CLI app

--- control/api_server/web/controllers/observe_controller.ex
+++ control/api_server/web/controllers/observe_controller.ex
@@ -1,30 +1,40 @@
 defmodule ApiServer.ObserveController do
   use ApiServer.Web, :controller
   import ApiServer.ValidationPlug, only: [valid_observe_level: 2]
+  import ApiServer.ExtractionPlug, only: [extract_targeting: 2]
+  import UUID, only: [uuid4: 0]
+  import ApiServer.TargetingUtils, only: [summarize_targeting: 1]
 
   plug :valid_observe_level when action in [:observe]
+  plug :extract_targeting when action in [:observe]
 
-  def observe(conn, _) do
-    json conn_with_status(conn, 501), conn_json(conn, 501)
-  end
+  def observe(%Plug.Conn{params: %{"level" => level}} = conn, _) do
 
-  def observe(conn, _, _) do
-    json conn_with_status(conn, 501), conn_json(conn, 501)
-  end
-
-  defp conn_with_status(conn, stat) do
     conn
-    |> put_status(stat)
+      |> put_status(200)
+      |> json(
+          Map.merge(
+            %{
+              error: :false,
+              level: level,
+              timestamp: DateTime.to_string(DateTime.utc_now()),
+              actions: generate_actions()
+            },
+            summarize_targeting(conn.assigns)
+          )
+         )
   end
 
-  defp conn_json(conn, code) do
-    handled_by = controller_module(conn)
+  defp generate_actions() do
+    Enum.map(1..:rand.uniform(10), fn (x) -> generate_action() end)
+  end
+
+  defp generate_action() do
     %{
-      path: current_path(conn),
-      action: action_name(conn),
-      status_code: code,
-      msg: "Not Implemented - Handled by #{handled_by}",
-      error: :true
+      sensor: uuid4(),
+      virtue: uuid4(),
+      level: Enum.random(["off", "default", "low", "high", "adversarial"])
     }
   end
+
 end
