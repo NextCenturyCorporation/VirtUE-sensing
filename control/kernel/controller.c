6c9318fba576a1c5fea08d70dd4599cb18e732c9
Mike Day <ncmike@ncultra.org>
2017-11-09 18:31:39 -0500

    very early stub of a controller

--- control/kernel/controller.c
+++ control/kernel/controller.c
@@ -2,3 +2,25 @@
  * in-virtue kernel controller
  * Published under terms of the Gnu Public License v2, 2017
  ******************************************************************************/
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include "controller.h"
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("In-VirtUE Kernel Controller");
+
+static int __init controller_init(void)
+{
+    printk(KERN_INFO "Hello world!\n");
+    return 0;
+}
+
+static void __exit hello_cleanup(void)
+{
+
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
