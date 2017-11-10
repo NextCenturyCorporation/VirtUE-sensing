1f73e002d739cc8ecbfb55c637293aa3541e6bd0
Mike Day <ncmike@ncultra.org>
2017-11-10 10:14:47 -0500

    the stub can load

--- control/kernel/controller.c
+++ control/kernel/controller.c
@@ -2,10 +2,10 @@
  * in-virtue kernel controller
  * Published under terms of the Gnu Public License v2, 2017
  ******************************************************************************/
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/printk.h>
 #include "controller.h"
 
 MODULE_LICENSE("GPL");
@@ -13,14 +13,14 @@ MODULE_DESCRIPTION("In-VirtUE Kernel Controller");
 
 static int __init controller_init(void)
 {
-    printk(KERN_INFO "Hello world!\n");
+    printk(KERN_INFO "In-kernel probe controller\n");
     return 0;
 }
 
-static void __exit hello_cleanup(void)
+static void __exit controller_cleanup(void)
 {
 
 }
 
-module_init(hello_init);
-module_exit(hello_cleanup);
+module_init(controller_init);
+module_exit(controller_cleanup);
--- control/kernel/controller.h
+++ control/kernel/controller.h
@@ -2,6 +2,4 @@
  * in-virtue kernel controller
  * Published under terms of the Gnu Public License v2, 2017
  ******************************************************************************/
-
-#include <include/linux/printk.h>
 #include <kernel/trace/trace.h>
