564fe11c7e093909452d2ce066ecd26917c1e49e
Mike Day <ncmike@ncultra.org>
2017-11-13 18:01:47 -0500

    Define what a sensor is vis-a-vis a probe. create a 'null' probe that is guaranteed to be part of every sensor. Use the sensor struct to organize probes that are related to each other.

--- control/kernel/controller.c
+++ control/kernel/controller.c
@@ -7,7 +7,9 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("In-VirtUE Kernel Controller");
 
-LIST_HEAD(active_probes);
+LIST_HEAD(active_sensors);
+struct list_head *probe_queues[0x10];
+
 int probe_socket;
 
 /* template probe function  */
@@ -16,13 +18,15 @@ int _pr(uint64_t flags, uint8_t *probe_data)
 	return 0;
 }
 
-struct probe_s p = {.id="probe-controller",
-					.lock=__SPIN_LOCK_UNLOCKED(lock),
-					.probe=_pr};
+struct kernel_sensor ks = {.id="kernel-sensor",
+						   .lock=__SPIN_LOCK_UNLOCKED(lock),
+						   .probes[0].id="probe-controller",
+						   .probes[0].lock=__SPIN_LOCK_UNLOCKED(lock),
+						   .probes[0].probe=_pr};
 
 static int __init controller_init(void)
 {
-    printk(KERN_INFO "In-kernel probe controller, %s\n", p.id);
+    printk(KERN_INFO "In-kernel probe controller, %s\n", ks.probes[0].id);
     return 0;
 }
 
--- control/kernel/controller.h
+++ control/kernel/controller.h
@@ -11,9 +11,33 @@
 #include <linux/init.h>
 #include <linux/printk.h>
 #include <linux/spinlock.h>
-#include <kernel/trace/trace.h>
+/*#include <trace/ftrace.h>*/
 #include <linux/list.h>
 #include <linux/socket.h>
+#include <linux/kthread.h>
+
+
+/* - - probe - -
+ *
+ * A probe is an active sensor. It is a function that runs as a kernel
+ * task at regular intervals to obtain information or to check the
+ * truth of various assertions.
+
+ * It's expected that most sensors will be comprised of one or more
+ * probes; that a sensor is not always an active probe and a probe is
+ * not always a sensor.
+ *
+ * By defining a passive sensor as a special kind of probe--a
+ * "passive probe"--we can say that a sensor is always also probe.
+ *
+ * A Sensor is also one or more passive or active probes.
+ *
+ * At the prototype stage, a sensor will have a fixed array of
+ * un-ininitialized probes, and those probes will be initiailized as
+ * they are registered with the kernel.
+ */
+
+#define PROBES_PER_SENSE 128
 
 /* prototype probe routine */
 int (*probe)(uint64_t flags, uint8_t *buf) = NULL;
@@ -27,19 +51,34 @@ struct probe_s {
 	uint8_t data[DEFAUT_PROBE_DATA];
 };
 
-int register_probe(uint64_t flags,
+uint8_t *register_probe(uint64_t flags,
 				   int (*probe)(uint64_t, uint8_t *),
 				   int delay, int timeout, int repeat);
 int unregister_probe(uint8_t *probe_id);
 
 /* allocates and returns a buffer with probe data */
 uint8_t *list_probes(int level, uint8_t *filter);
+
 int set_probe_level(int level, uint8_t *filter);
+
 /* retrieve the probe to the client or set to the server, */
 /* the probe data structure */
 int retrieve_probe(uint8_t *probe_id,
 				   uint8_t *results_buf,
 				   int buf_size);
+
 uint64_t update_probe(uint8_t *probe_id,
 					  uint8_t *update,
 					  int update_size);
+
+struct kernel_sensor {
+	uint8_t *id;
+	spinlock_t lock;
+	uint64_t flags;
+	struct list_head *l;
+	struct probe_s probes[PROBES_PER_SENSE];
+};
+
+uint8_t *register_sensor(struct kernel_sensor *s);
+int unregister_sensor(uint8_t *sensor_id);
+uint8_t *list_sensors(uint8_t *filter);
