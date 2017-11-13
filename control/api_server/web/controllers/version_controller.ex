f2d24eda5e66f045aaf8223d0a7bfd85e77ab1ec
Patrick Dwyer <patrick.dwyer@invincea.com>
2017-11-13 10:57:38 -0500

    Added module doc and function doc

--- control/api_server/web/controllers/version_controller.ex
+++ control/api_server/web/controllers/version_controller.ex
@@ -1,6 +1,12 @@
 defmodule ApiServer.VersionController do
+  @moduledoc """
+  Versioning and non-authenticated system status.
+  """
   use ApiServer.Web, :controller
 
+  @doc """
+  Retrieve the version of the ApiServer code base.
+  """
   def version(conn, _) do
     conn
     |> put_status(200)
