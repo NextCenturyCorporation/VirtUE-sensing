e7dafa9b8132fcf6ec2a2fd055410f0773752199
Mike Day <ncmike@ncultra.org>
2017-11-14 09:43:38 -0500

    start and stop an independent thread to manage the probes, the probe socket, and the kernel probe and sensor.

--- control/kernel/controller.c
+++ control/kernel/controller.c
@@ -9,7 +9,7 @@ MODULE_DESCRIPTION("In-VirtUE Kernel Controller");
 
 LIST_HEAD(active_sensors);
 struct list_head *probe_queues[0x10];
-
+struct task_struct *probe_task;
 int probe_socket;
 
 /* template probe function  */
@@ -24,15 +24,29 @@ struct kernel_sensor ks = {.id="kernel-sensor",
 						   .probes[0].lock=__SPIN_LOCK_UNLOCKED(lock),
 						   .probes[0].probe=_pr};
 
+static int run_controller(void *__unused)
+{
+	while (!kthread_should_stop()) {
+		schedule_timeout_interruptible(HZ*30);
+	}
+	return 0;
+}
+
+
+
 static int __init controller_init(void)
 {
-    printk(KERN_INFO "In-kernel probe controller, %s\n", ks.probes[0].id);
-    return 0;
+	probe_task = kthread_create(run_controller, NULL, "run-kernel-probes");
+	if (!IS_ERR(probe_task))
+		wake_up_process(probe_task);
+	else
+		WARN_ON(1);
+	return 0;
 }
 
 static void __exit controller_cleanup(void)
 {
-
+	kthread_stop(probe_task);
 }
 
 module_init(controller_init);
