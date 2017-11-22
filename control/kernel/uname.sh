a9a49fb213457124d9a46038c410fdc4753267c7
Mike Day <mike.day@twosixlabs.com>
2017-11-22 14:19:51 -0500

    ok for now I'm happy with the header file this generates. If we need any other compile-time variables from the host we can add some more.

--- control/kernel/uname.sh
+++ control/kernel/uname.sh
@@ -9,15 +9,16 @@ IFS=$OLDIFS
 echo $VERSION $SHORT_VERSION
 
 
+echo "/* this file is generated automatically in the makefile */" > $FILENAME
+echo "const char *cont_long_version = \"$VERSION\";" >> $FILENAME
+echo "const char *cont_short_version = \"$SHORT_VERSION\";" >> $FILENAME
+
 if [[ $SHORT_VERSION < "4.13.1" ]] ; then
     API=old
+    echo "#define OLD_API=1;" >> $FILENAME
 else
     API=new
+    echo "#define NEW_API=1;" >> $FILENAME
 fi
-
+echo "const char *cont_api = \"$API\";" >> $FILENAME
 echo $API
-
-echo "/* this file is generated automatically in the makefile */" > $FILENAME
-echo "const char *long_version = \"$VERSION\";" >> $FILENAME
-echo "const char *short_version = \"$SHORT_VERSION\";" >> $FILENAME
-echo "const char *api = \"$API\";" >> $FILENAME
