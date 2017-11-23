13f55b705e8e1c8606e9e883d5043300f4af1009
Mike Day <mike.day@twosixlabs.com>
2017-11-23 10:50:37 -0500

    uname.sh now captures seperately components of the linux kernel version. The generated header now includes variables for version, patchlevel, and sublevel, as they are defined by the linux kernel kbuilld system.

--- control/kernel/uname.sh
+++ control/kernel/uname.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
 FILENAME=uname.h
-VERSION=$(uname -r)
+FULL_VERSION=$(uname -r)
 OLDIFS=$IFS
 IFS='-'
-SHORT_VERSION=$(echo $VERSION | awk '{print $1}')
-IFS=$OLDIFS
-echo $VERSION $SHORT_VERSION
+SHORT_VERSION=$(echo $FULL_VERSION | awk '{print $1}')
 
+echo $FULL_VERSION $SHORT_VERSION
+
+IFS='.'
+VERSION=$(echo $SHORT_VERSION | awk '{print $1}')
+PATCHLEVEL=$(echo $SHORT_VERSION | awk '{print $2}')
+SUBLEVEL=$(echo $SHORT_VERSION | awk '{print $3}')
+
+IFS=$OLDIFS
 
 echo "/* this file is generated automatically in the makefile */" > $FILENAME
-echo "const char *cont_long_version = \"$VERSION\";" >> $FILENAME
+echo "const char *cont_long_version = \"$FULL_VERSION\";" >> $FILENAME
 echo "const char *cont_short_version = \"$SHORT_VERSION\";" >> $FILENAME
+echo "const int version = $VERSION;" >> $FILENAME
+echo "const int patchlevel = $PATCHLEVEL;" >> $FILENAME
+echo "const int sublevel = $SUBLEVEL;" >> $FILENAME
 
-if [[ $SHORT_VERSION < "4.9" ]] ; then
-    API=old
-    echo "#define OLD_API = 1;" >> $FILENAME
-else
-    API=new
-    echo "#define NEW_API = 1;" >> $FILENAME
+
+if (( $VERSION >= 4 )) ; then
+    if (( $PATCHLEVEL < 9 )); then
+	API=old
+	echo "#define OLD_API 1;" >> $FILENAME
+    else
+	API=new
+	echo "#define NEW_API 1;" >> $FILENAME
+    fi
 fi
+
 echo "const char *cont_api = \"$API\";" >> $FILENAME
 echo $API
