547396d49870fd3708ef3a5371b2ffafc689e856
Mike Day <mike.day@twosixlabs.com>
2017-11-25 18:24:30 -0500

    Added a proper destroy function for a struct kthread_work that is part of a probe. It will flush the work noded from any work_list it is on, zero the kthread_work, and return a void *

--- control/kernel/uname.sh
+++ control/kernel/uname.sh
@@ -31,11 +31,13 @@ if (( $VERSION >= 4 )) ; then
 	echo "#define OLD_API 1" >> $FILENAME
 	echo "#define CONT_INIT_WORK init_kthread_work"  >> $FILENAME
 	echo "#define CONT_INIT_WORKER init_kthread_worker"  >> $FILENAME
+	echo "#define CONT_FLUSH_WORK flush_kthread_work" >> $FILENAME
 	echo "#define CONT_QUEUE_WORK queue_kthread_work"  >> $FILENAME
     else
 	API=new
 	echo "#define NEW_API 1" >> $FILENAME
 	echo "#define CONT_INIT_WORK kthread_init_work"  >> $FILENAME
+	echo "#define CONT_FLUSH_WORK kthread_flush_work" >> $FILENAME
 	echo "#define CONT_QUEUE_WORK kthread_queue_work"  >> $FILENAME
         echo "#define CONT_INIT_WORKER init_kthread_worker"  >> $FILENAME
     fi
