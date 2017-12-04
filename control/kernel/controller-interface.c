00b44af64481e05b0d78f19190e7847c17e50b60
Mike Day <mike.day@twosixlabs.com>
2017-12-04 13:30:47 -0500

    add a new file for the linux sockets interface

---
+++ control/kernel/controller-interface.c
@@ -0,0 +1,9 @@
+/*******************************************************************************
+ * in-virtue kernel controller
+ * Published under terms of the Gnu Public License v2, 2017
+ ******************************************************************************/
+
+#include "controller.h"
+#include "jsmn/jsmn.h"
+
+/* hold socket JSON probe interface */
