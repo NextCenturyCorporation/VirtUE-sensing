d78bdeed787a19ba8d136cdd9f7ffd6dedc881e0
Mike Day <mike.day@twosixlabs.com>
2018-02-26 19:42:32 -0500

    add getopt_long_only and rough out help options

--- control/kernel/strscp.c
+++ control/kernel/strscp.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <getopt.h>
 
 #define GFP_KERNEL 1
 #define krealloc(p, t, f)					\
 				 realloc((p), (t))
+#define DMSG printf
 
 char *test1 = "this is terminated by a new line\n trailing";
 typedef char uint8_t;
@@ -112,7 +114,93 @@ uint8_t *escape_newlines(uint8_t *in, int len)
 	return in;
 }
 
-enum type { ADD_NL, TRIM_TO_NL, UNXP_NL, XP_NL };
+void usage(void)
+{
+	DMSG("help\n");
+}
+
+
+enum type { ADD_NL, TRIM_TO_NL, UXP_NL, XP_NL };
+int add_flag, trim_flag, uxp_flag, xp_flag;
+
+
+static inline void
+get_options (int argc, char **argv)
+{
+	while (1)
+    {
+		if (argc < 2)
+			usage ();
+
+		int c;
+		static struct option long_options[] = {
+			{"dummy-for-short-option", no_argument, NULL, 0},
+			{"add-nl", required_argument, &add_flag, 1},
+			{"trim-to-nl", no_argument, &trim_flag, 1},
+			{"unescape-nl", required_argument, &uxp_flag, 1},
+			{"escape-nl", required_argument, &xp_flag, 1},
+			{"help", no_argument, NULL, 0},
+			{0, 0, 0, 0}
+		};
+		int option_index = 0;
+		c = getopt_long_only (argc, argv, "ila:f:u:s:dh",
+							  long_options, &option_index);
+		if (c == -1)
+		{
+			break;
+		}
+
+    restart_long:
+		switch (option_index)
+		{
+			DMSG("selected option %s\n", long_options[option_index].name);
+
+		case 0:
+			switch (c)
+			{
+			case 'a':
+				option_index = 1;
+				add_flag = 1;
+				goto restart_long;
+			case 't':
+				option_index = 2;
+				trim_flag = 1;
+				goto restart_long;
+			case 'u':
+				option_index = 3;
+				uxp_flag = 1;
+				goto restart_long;
+			case 'e':
+				option_index = 4;
+				xp_flag = 1;
+				goto restart_long;
+			default:
+				option_index = 5;
+				usage ();
+			}
+		case 1:		/* info */
+		{
+
+		}
+		break;
+
+		case 2:		/* list */
+
+			break;
+		case 3:		/* find */
+		{
+			break;
+		}
+		case 4:		/* apply */
+		{
+			break;
+		}
+		case 5:		/* remove */
+		default:
+			break;
+		}
+    }
+}
 
 int main(int argc, uint8_t **argv)
 {
