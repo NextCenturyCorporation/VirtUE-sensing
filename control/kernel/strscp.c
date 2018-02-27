90691cec0bd86dcfe319a7319dde8244157b3700
Mike Day <mike.day@twosixlabs.com>
2018-02-27 13:25:26 -0500

    add jsmn json parser to strsep, update Makefile. now builds a test program that is also a JSONL parser

--- control/kernel/strscp.c
+++ control/kernel/strscp.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <assert.h>
 #include <getopt.h>
+#include "jsmn/jsmn.h"
 
 #define GFP_KERNEL 1
 #define krealloc(p, t, f)					\
 				 realloc((p), (t))
-#define DMSG printf
-
-char *test1 = "this is terminated by a new line\n trailing";
 typedef char uint8_t;
 const uint8_t escape [] = {0x5c, 0x00};
+
+int verbose_flag = 0;
 
 uint8_t *add_nl_at_end(uint8_t *in, int len)
 {
@@ -114,54 +115,92 @@ uint8_t *escape_newlines(uint8_t *in, int len)
 	return in;
 }
 
+uint8_t *empty(uint8_t *in, int len)
+{
+	return NULL;
+}
+
+uint8_t *(*function_table[])(uint8_t *, int) =
+{
+	empty,
+	add_nl_at_end,
+	trim_to_nl,
+	unescape_newlines,
+	escape_newlines
+};
+char *program_name;
 void usage(void)
 {
-	DMSG("help\n");
-	exit(1);
+	printf("%s --verbose <test>,", program_name);
+	printf (" where <test> is one of:\n");
+	printf ("\t --add-nl <string> --trim-to-nl <string> " \
+			"--unescape-nl <string> --escape-nl <string>\n");
+	exit(0);
 }
 
 
-enum type { ADD_NL = 0, TRIM_TO_NL, UXP_NL, XP_NL, USAGE };
-uint8_t *input_string = NULL;
+enum type { VERBOSE, ADD_NL, TRIM_TO_NL, UXP_NL, XP_NL, USAGE };
+uint8_t *input_string = NULL, *output_string = NULL;
 enum type option_index = USAGE;
+
 static inline int
 get_options (int argc, char **argv)
 {
 	if (argc < 2)
 		usage ();
+	while (1) {
+		int c;
+		static struct option long_options[] = {
+			{"verbose", no_argument, &verbose_flag, 1},
+			{"add-nl", required_argument, 0, 'a'},
+			{"trim-to-nl", required_argument, 0, 't'},
+			{"unescape-nl", required_argument, 0, 'u'},
+			{"escape-nl", required_argument, 0, 'x'},
+			{"help", no_argument, NULL, 0},
+			{0, 0, 0, 0}
+		};
 
-	int c;
-	static struct option long_options[] = {
-		{"add-nl", required_argument, 0, 'a'},
-		{"trim-to-nl", required_argument, 0, 't'},
-		{"unescape-nl", required_argument, 0, 'u'},
-		{"escape-nl", required_argument, 0, 'x'},
-		{"help", no_argument, NULL, 0},
-		{0, 0, 0, 0}
-	};
+		c = getopt_long_only (argc, argv, "va:t:u:e:h",
+							  long_options, (int *)&option_index);
+		if (c == -1)
+			break;
 
-	c = getopt_long_only (argc, argv, "a:t:u:e:h",
-						  long_options, (int *)&option_index);
-	if (c == -1)
-		usage();
+		switch(option_index) {
+		case VERBOSE:
+		{
+			break;
+		}
 
-	switch(option_index) {
+		case ADD_NL:		/* add a newline */
+		case TRIM_TO_NL:		/* trim to the first newline */
+		case UXP_NL:		/* un-escape escaped newlines */
+		case XP_NL:		{   /* escape newlines  */
+			int input_len, output_len;
+			input_string = strdup(optarg);
+			input_len = strlen(input_string);
+			output_string = function_table[option_index](input_string,
+														 input_len);
+			output_len = strlen(output_string);
+			if (verbose_flag) {
+				printf("option: %d\ninput %d: %s\noutput %d:%s\n",
+					   option_index,
+					   input_len, optarg,
+					   output_len, output_string);
+			}
+			break;
+		}
 
-	case ADD_NL:		/* add a newline */
-	case TRIM_TO_NL:		/* trim to the first newline */
-	case UXP_NL:		/* un-escape escaped newlines */
-	case XP_NL:		/* escape newlines  */
-		input_string = strdup(optarg);
-		return option_index;
-		break;
-	case USAGE:		/* help */
-	default:
-		usage();
+		case USAGE:		/* help */
+		default:
+			usage();
+		}
 	}
-}
 
+}
 
 int main(int argc, uint8_t **argv)
 {
+	int i;
+	program_name = strdup(argv[0]);
 	return get_options(argc, argv);
 }
