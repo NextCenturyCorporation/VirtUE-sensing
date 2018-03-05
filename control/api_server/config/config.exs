fda6bcd7303261d6355dd9f020d0a525f5f9edf7
Patrick Dwyer <patrick.dwyer@invincea.com>
2018-03-05 16:15:45 -0500

    Let's tear down the kafka interaction and rebuild it to debug

--- control/api_server/config/config.exs
+++ control/api_server/config/config.exs
@@ -52,37 +52,47 @@ config :api_server, ApiServer.Scheduler,
 
 # Kafka connections
 config :kafka_ex,
-
-  # brokers
   brokers: [
     {"kafka", 9455}
   ],
-
   consumer_group: "kafka_ex",
-
   disable_default_worker: true,
-
   sync_timeout: 30000,
-
-  max_restarts: 10,
-
-  max_seconds: 60,
-
+  max_restarts: 15,
   commit_interval: 5_000,
-
   commit_threshold: 100,
-
-  use_ssl: true,
-
-  ssl_options: [
-
-    cacertfile: "/app/certs/ca.pem",
-    certfile: "/app/certs/cert.pem",
-    keyfile: "/app/certs/cert-key.pem"
-
-  ],
-
   kafka_version: "0.9.0"
+  
+#  # brokers
+#  brokers: [
+#    {"kafka", 9455}
+#  ],
+#
+#  consumer_group: "kafka_ex",
+#
+#  disable_default_worker: true,
+#
+#  sync_timeout: 30000,
+#
+#  max_restarts: 10,
+#
+#  max_seconds: 60,
+#
+#  commit_interval: 5_000,
+#
+#  commit_threshold: 100,
+#
+#  use_ssl: true,
+#
+#  ssl_options: [
+#
+#    cacertfile: "/app/certs/ca.pem",
+#    certfile: "/app/certs/cert.pem",
+#    keyfile: "/app/certs/cert-key.pem"
+#
+#  ],
+#
+#  kafka_version: "0.9.0"
 
 
 
