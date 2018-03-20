f0ec721c44b63632c3dd77622964661e29348108
markfsanderson <mark.sanderson@twosixlabs.com>
2018-03-20 14:44:03 -0500

    added new sensor and corrected config file

---
+++ sensors/sensor_tasklist/run_tasklist_sensor.ps1
@@ -0,0 +1,2 @@
+
+python c\opt\sensors\tasklist\sensor_tasklist.py --public-key-path c:\opt\sensors\tasklist\certs\rsa_key.pub --private-key-path c:\opt\sensors\tasklist\certs\rsa_key --ca-key-path c:\opt\sensors\tasklist\certs\ --api-host api --sensor-port 11020
