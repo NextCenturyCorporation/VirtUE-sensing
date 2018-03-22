6ac6ae43177f2154cc16b5646637ed30b771578a
markfsanderson <mark.sanderson@twosixlabs.com>
2018-03-22 15:33:59 -0500

    update wip

--- sensors/sensor_tasklist/run_tasklist_sensor.ps1
+++ sensors/sensor_tasklist/run_tasklist_sensor.ps1
@@ -1,2 +1,3 @@
+echo "Running Sensor TaskList . . ."
 
-python c\opt\sensors\tasklist\sensor_tasklist.py --public-key-path c:\opt\sensors\tasklist\certs\rsa_key.pub --private-key-path c:\opt\sensors\tasklist\certs\rsa_key --ca-key-path c:\opt\sensors\tasklist\certs\ --api-host api --sensor-port 11020
+Start-Process -FilePath python.exe -ArgumentList "c:\opt\sensors\tasklist\sensor_tasklist.py --public-key-path c:\opt\sensors\tasklist\certs\rsa_key.pub --private-key-path c:\opt\sensors\tasklist\certs\rsa_key --ca-key-path c:\opt\sensors\tasklist\certs\ --api-host api --sensor-port 11020"
