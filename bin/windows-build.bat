42619093d0ee21976492c7f76f32a0c3fb5dd021
markfsanderson <mark.sanderson@twosixlabs.com>
2018-03-22 16:28:13 -0500

    more WIP

--- bin/windows-build.bat
+++ bin/windows-build.bat
@@ -1,6 +1,5 @@
 @echo off
-
-@ECHO base system setup and requirements
+@ECHO Configuring execution environment . . .
 SET WORKDIR=C:\app
 SET TEMP=%SystemDrive%\Temp
 SET PYTHONUNBUFFERED=0
@@ -10,22 +9,22 @@ SET POWERSHELL=powershell -NoProfile -ExecutionPolicy Bypass
 MKDIR %WORKDIR%
 MKDIR %TEMP%
 
-@ECHO Download and install Visual Studio 2017 Build Kit w/2015 Components
+@ECHO Download and Install Visual Studio 2017 Build Kit w/2015 Components . . .
 %POWERSHELL% Invoke-WebRequest -Uri "https://aka.ms/vs/15/release/vs_BuildTools.exe" -OutFile %TEMP%\vs_BuildTools.exe 
 %TEMP%\vs_BuildTools.exe --quiet --wait --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Component.VC.140 --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest --includeRecommended 
 DEL /F /Q %TEMP%\vs_BuildTools.exe
 
-@ECHO Download and install python
+@ECHO Download and install python . . . 
 %POWERSHELL% Invoke-WebRequest -Uri "https://www.python.org/ftp/python/%PYTHONVER%/python-%PYTHONVER%.exe" -OutFile %TEMP%\python-%PYTHONVER%.exe 
 %TEMP%\python-%PYTHONVER%.exe -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1 TargetDir=%SystemDrive%\Python%PYTHONVER% CompileAll=1' -Wait 
 DEL /F /Q %TEMP%\python-%PYTHONVER%.exe
-SET PATH=%SystemDrive%\Python%PYTHONVER%\Scripts;%SystemDrive%\Python%PYTHONVER%;%PATH
+SET PATH=%SystemDrive%\Python%PYTHONVER%\Scripts;%SystemDrive%\Python%PYTHONVER%;%PATH%
 python -m pip install --upgrade pip
 
 @ECHO Go to the windows target directory from .\savior
 PUSHD targets\win-target
 
-@ECHO Installing REQUIREMENTS.TXT install and run
+@ECHO Installing REQUIREMENTS.TXT install and run . . . 
 MKDIR %SystemDrive%\app\requirements
 XCOPY /Y /S /F /V requirements\*.* %SystemDrive%\app\requirements\
 %SystemDrive%\Python%PYTHONVER%\scripts\pip.exe install -r %SystemDrive%\app\requirements\requirements_master.txt
