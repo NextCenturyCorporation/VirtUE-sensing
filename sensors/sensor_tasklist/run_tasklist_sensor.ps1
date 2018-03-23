32da72904f1c114257f45c39de6e04bf581b4f20
markfsanderson <mark.sanderson@twosixlabs.com>
2018-03-23 17:01:37 -0500

    fixed up the last bits

--- sensors/sensor_tasklist/run_tasklist_sensor.ps1
+++ sensors/sensor_tasklist/run_tasklist_sensor.ps1
@@ -1,5 +1,7 @@
 echo "Running Sensor TaskList . . ."
 
+pushd c:\opt\sensors\tasklist
 Start-Process -FilePath python.exe -ArgumentList ".\gen_key.py" -Wait
+popd
 
 Start-Process -FilePath python.exe -ArgumentList "c:\opt\sensors\tasklist\sensor_tasklist.py --public-key-path c:\opt\sensors\tasklist\certs\rsa_key.pub --private-key-path c:\opt\sensors\tasklist\certs\rsa_key --ca-key-path c:\opt\sensors\tasklist\certs\ --api-host api --sensor-port 11020" -WindowStyle Normal
