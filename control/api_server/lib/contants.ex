6b98168a82907fde967544de24f581edb36b9523
Patrick Dwyer <patrick.dwyer@invincea.com>
2018-03-30 14:41:46 -0400

    Fixed issue with sensor_os in constants, fixed issue with missing :nxdomain error condition, added better interpolated error message to avoid further issues

--- control/api_server/lib/contants.ex
+++ control/api_server/lib/contants.ex
@@ -1,6 +1,10 @@
 defmodule ApiServer.Constants do
+  @moduledoc """
+  Useful constants that shouldn't be litered around the system.
+  """
 
   def sensor_os() do
     ["linux", "windows", "nt", "rump"]
   end
+
 end
