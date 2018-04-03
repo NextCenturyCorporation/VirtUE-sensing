d7335566f3d9ac448cd82eb5c620ae46f9dffade
Mike Day <mike.day@twosixlabs.com>
2018-04-03 11:32:22 -0400

    update cycle.sh to reflect a single kernel module

--- control/kernel/cycle.sh
+++ control/kernel/cycle.sh
@@ -5,15 +5,10 @@ sudo insmod ./controller.ko ps_repeat=1 ps_timeout=1
 read -n 1 -s -r -p "Press any key to continue"
 echo ""
 
-sudo insmod ./controller-interface.ko
-read -n 1 -s -r -p "Press any key to continue"
-echo ""
-
-#sudo cat /var/log/messages | grep kernel-ps
 sudo dmesg | grep kernel-ps
 sudo lsmod | grep controller
 echo ""
 read -n 1 -s -r -p "Press any key to continue"
 echo ""
-sudo rmmod controller_interface
 sudo rmmod controller
+git
