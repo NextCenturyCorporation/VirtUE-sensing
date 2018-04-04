e1b713d81c1da61da78b3f52c5caaf659b8e2fcc
Matt Leinhos <matt.leinhos@twosixlabs.com>
2018-04-04 11:20:40 -0500

    Removed staged files that were committed.

--- targets/demo-target/sensor_startup/run_lsof_sensor.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python /opt/sensors/lsof/sensor_lsof.py --public-key-path /opt/sensors/lsof/certs/rsa_key.pub --private-key-path /opt/sensors/lsof/certs/rsa_key --ca-key-path /opt/sensors/lsof/certs/ --api-host sensing-api.savior.internal --sensor-port 11020
