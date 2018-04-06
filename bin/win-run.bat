a8213985ad65ca72d2bb5c8cd306b8b30a1b49ff
Mark F Sanderson <mark.sanderson@twosixlabs.com>
2018-04-06 22:33:25 +0000

    wip generic runner batch file

--- bin/win-run.bat
+++ bin/win-run.bat
@@ -1,5 +1,7 @@
 @echo off
-pushd c:\opt\sensors\tasklist
+set SENSORNAME=processlist
+set PORT=11021
+pushd c:\opt\sensors\%SENSORNAME%
 del /f certs\rsa_key 
-python c:\opt\sensors\tasklist\sensor_tasklist.py --public-key-path c:\opt\sensors\tasklist\certs\rsa_key.pub --private-key-path c:\opt\sensors\tasklist\certs\rsa_key --ca-key-path c:\opt\sensors\tasklist\certs\ --api-host sensing-api.savior.internal --sensor-port 11020
+python c:\opt\sensors\%SENSORNAME%\sensor_%SENSORNAME%.py --public-key-path c:\opt\sensors\%SENSORNAME%\certs\rsa_key.pub --private-key-path c:\opt\sensors\%SENSORNAME%\certs\rsa_key --ca-key-path c:\opt\sensors\%SENSORNAME%\certs\ --api-host sensing-api.savior.internal --sensor-port %PORT%
 popd
