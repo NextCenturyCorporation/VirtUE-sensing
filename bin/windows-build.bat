2c815c3daae26fc0b9d3c7a7f1f8f69687ef528b
markfsanderson <mark.sanderson@twosixlabs.com>
2018-04-11 13:51:09 -0500

    updating batchfiles for new handlelist sensor and adding batch file to run all sensors

--- bin/windows-build.bat
+++ bin/windows-build.bat
@@ -52,9 +52,8 @@ COPY /Y run.ps1 %SystemDrive%\app
 @ECHO POP back to .\savior
 POPD
 
-@ECHO Opening Port 11020 in the firewall for sensor communications
+@ECHO Opening Port 11020 - 11022 in the firewall for sensor communications
 netsh advfirewall firewall add rule name="Open Port 11020" dir=in action=allow protocol=TCP localport=11020
 netsh advfirewall firewall add rule name="Open Port 11021" dir=in action=allow protocol=TCP localport=11021
+netsh advfirewall firewall add rule name="Open Port 11022" dir=in action=allow protocol=TCP localport=11022
 
-@ECHO Running Sensors . . .
-%POWERSHELL% %SystemDrive%\app\run.ps1
