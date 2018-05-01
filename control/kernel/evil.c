112afed2358345a71d8837314e4bcb702f5e2c85
Mike Day <mike.day@twosixlabs.com>
2018-05-01 11:24:40 -0400

    report on block size and number of blocks

--- control/kernel/evil.c
+++ control/kernel/evil.c
@@ -8,6 +8,7 @@
 #include <asm/uaccess.h>
 #include <linux/printk.h>
 #include <linux/vmalloc.h>
+#include "import-header.h"
 
 char *filename = "/proc/1/mounts";
 module_param(filename, charp, 0644);
@@ -29,6 +30,23 @@ module_param(filename, charp, 0644);
  *
  * "buflen" should be positive.
  */
+
+#ifdef NOTHING
+static inline unsigned long
+get_fd_from_sys_open(char *name)
+{
+	unsigned long __fd;
+	mm_segment_t old_fs;
+
+	old_fs = get_fs();
+	set_fs(get_ds());
+	printk("calling sys_open\n");
+
+	__fd = IMPORTED(sys_open)((char __user *)name, O_RDONLY, 0);
+	set_fs(old_fs);
+	return __fd;
+}
+#endif
 
 static inline struct fd
 get_fd_from_file(unsigned long v)
@@ -69,7 +87,11 @@ file_getattr(char *name)
 	if (f) {
 		ccode = vfs_getattr(&f->f_path, &k_stat, mask, flags);
 		printk("evil-vfs_getattr returned %d\n", ccode);
-		printk("%s size: %lld\n", name, k_stat.size);
+		printk("%s size: %lld, blksize: %x, blks: %llx\n",
+			   name,
+			   k_stat.size,
+			   k_stat.blksize,
+			   k_stat.blocks);
 		filp_close(f, 0);
 	}
 
@@ -80,29 +102,36 @@ file_getattr(char *name)
 
 static int read_file(char *name)
 {
-  static char __data[0x1000];
-  loff_t size = 0L;
-  int ccode;
-  struct file *f;
-  struct fd *fd;
-  struct fd __fd;
+	static char __data[0x1000];
+	loff_t size = 0L;
+	int ccode;
+	struct file *f;
+	struct fd *fd;
+	struct fd __fd;
+//	unsigned long fd_sys_open;
 
-  f = filp_open(name, O_RDONLY, 0);
-  if (f) {
-	  printk("we have a file pointer %p\n", f);
-	  printk("evil-flags %x\n", f->f_flags);
-	  ccode = kernel_read(f, &__data[0], 0x100, &size);
-	  printk("evil-ccode is %d, size is %llx\n", ccode, size);
-	  if (ccode < 0) {
-		   pr_err("Unable to open file: %s (%d)", name, ccode);
-		   filp_close(f, 0);
-		   return ccode;
-	  }
-	  printk("%s\n", (char *)__data);
-	  fd = get_struct_fd(f);
-	  __fd = get_fd_from_file((unsigned long)f);
+	f = filp_open(name, O_RDONLY, 0);
+	if (f) {
+		printk("we have a file pointer %p\n", f);
+		printk("evil-flags %x\n", f->f_flags);
+		ccode = kernel_read(f, &__data[0], 0x100, &size);
+		printk("evil-ccode is %d, size is %llx\n", ccode, size);
+		if (ccode < 0) {
+			pr_err("Unable to open file: %s (%d)", name, ccode);
+			filp_close(f, 0);
+			return ccode;
+		}
+		printk("%s\n", (char *)__data);
+		fd = get_struct_fd(f);
+		__fd = get_fd_from_file((unsigned long)f);
+		filp_close(f, 0);
 
-	  filp_close(f, 0);
+#ifdef NOTHING
+		fd_sys_open = get_fd_from_sys_open(name);
+		__fd = get_fd_from_file(fd_sys_open);
+		IMPORTED(sys_close)(fd_sys_open);
+#endif
+
   }
   return ccode;
 }
@@ -111,6 +140,8 @@ static int __init init(void)
 {
   read_file(filename);
   file_getattr(filename);
+
+
 
   return 0;
 }
