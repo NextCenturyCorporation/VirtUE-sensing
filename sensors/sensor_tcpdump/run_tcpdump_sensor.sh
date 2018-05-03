4bbd8f9dbe1fc5684735e0c7fb078c52b2cc5852
Patrick Dwyer <patrick.dwyer@invincea.com>
2018-05-03 10:08:54 -0400

    Fix run script for tcpdump

--- sensors/sensor_tcpdump/run_tcpdump_sensor.sh
+++ sensors/sensor_tcpdump/run_tcpdump_sensor.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python /opt/sensors/tcpdump/sensor_tcpdump.py --public-key-path /opt/sensors/lsof/certs/rsa_key.pub --private-key-path /opt/sensors/lsof/certs/rsa_key --ca-key-path /opt/sensors/lsof/certs/ --api-host api --sensor-port 11003
+python /opt/sensors/tcpdump/sensor_tcpdump.py --public-key-path /opt/sensors/tcpdump/certs/rsa_key.pub --private-key-path /opt/sensors/tcpdump/certs/rsa_key --ca-key-path /opt/sensors/tcpdump/certs/ --api-host sensing-api.savior.internal --sensor-port 11003
