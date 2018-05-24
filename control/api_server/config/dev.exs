e1a83d692c88da9eb9b9e7661bd2a56e9633f430
Patrick Dwyer <patrick.dwyer@invincea.com>
2018-05-24 11:39:52 -0400

    Get glyphicons properly working with static asset pipeline

--- control/api_server/config/dev.exs
+++ control/api_server/config/dev.exs
@@ -34,7 +34,7 @@ config :api_server, ApiServer.Endpoint,
 config :api_server, ApiServer.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg|eot|ttf|woff|woff2)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
       ~r{web/templates/.*(eex)$}
