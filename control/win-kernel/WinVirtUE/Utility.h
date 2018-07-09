f8acd091ac581b1d8aa8013c1346a2ea8f65ab54
markfsanderson <mark.sanderson@twosixlabs.com>
2018-07-09 18:38:29 -0500

    WIP decorate SAL w/IRQL requirements

--- control/win-kernel/WinVirtUE/Utility.h
+++ control/win-kernel/WinVirtUE/Utility.h
@@ -6,7 +6,8 @@
 */
 #pragma once
 #include "common.h"
-
+_IRQL_always_function_max_(DISPATCHER_LEVEL)
+_IRQL_always_function_min_(PASSIVE_LEVEL)
 _Check_return_
 _Success_(0 == return)
 LONG CompareAnsiString(_In_ CONST ANSI_STRING& string1, 
