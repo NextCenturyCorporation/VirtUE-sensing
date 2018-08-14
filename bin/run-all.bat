0db3ab77b1cd8b5c29290c5bc0da74e512b0d8ed
Mark Sanderson <mark@markfsanderson.net>
2018-08-14 15:07:30 -0500

    remove uneeded batch files

--- bin/run-all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-@ECHO Running All Sensors . . .
-SET POWERSHELL=powershell -NoProfile -ExecutionPolicy Bypass 
-%POWERSHELL% %SystemDrive%\app\run.ps1
--- bin/run-sensor.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set SENSORNAME=handlelist
-set PORT=11022
-pushd c:\opt\sensors\%SENSORNAME%
-del /f certs\rsa_key 
-python c:\opt\sensors\%SENSORNAME%\sensor_%SENSORNAME%.py --public-key-path c:\opt\sensors\%SENSORNAME%\certs\rsa_key.pub --private-key-path c:\opt\sensors\%SENSORNAME%\certs\rsa_key --ca-key-path c:\opt\sensors\%SENSORNAME%\certs\ --api-host sensing-api.savior.internal --sensor-port %PORT%
-popd
