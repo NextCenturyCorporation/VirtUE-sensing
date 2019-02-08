4ed862dcbfd54c36adcc37ae7c29c3c64c53ab33
Anthony Wong <anthony.wong@twosixlabs.com>
2019-02-08 13:30:37 -0800

    Fixed directories in run-local script

--- control/virtue-security/run-local.sh
+++ control/virtue-security/run-local.sh
@@ -2,8 +2,10 @@
 
 export CFSSL_SHARED_SECRET=de1069ab43f7f385d9a31b76af27e7620e9aa2ad5dccd264367422a452aba67f
 
+mkdir certs
+
 echo "Getting Client Certificate"
 python ./tools/get_certificates.py --cfssl-host sensing-ca.savior.internal --hostname api -d certs --quiet
 
 echo "Running virtue-security"
-python virtue-security --public-key-path /usr/src/app/certs/cert.pem "$@" --ca-key-path /usr/src/app/certs/ca.pem --private-key-path /usr/src/app/certs/cert-key.pem
+python virtue-security --public-key-path certs/cert.pem "$@" --ca-key-path certs/ca.pem --private-key-path certs/cert-key.pem
